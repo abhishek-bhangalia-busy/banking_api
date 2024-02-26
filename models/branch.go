91f57151539d4df352a3119943dab49d4ddf55ab
abhishekBhangalia <abhangalia0001@gmail.com>
2024-02-26 23:16:24 +0530

    fixed bugs

--- models/branch.go
+++ models/branch.go
@@ -4,7 +4,7 @@ package models
 type Branch struct {
 	ID       uint64 `pg:",pk"`
 	Address  string
-	BankID   uint64     `pg:"on_delete:CASCADE, notnull"`
+	BankID   uint64     `pg:"on_delete:CASCADE, notnull, on_update:CASCADE"`
 	Bank     *Bank      `pg:"rel:has-one"`
 	Accounts []*Account `pg:"rel:has-many"`
 }
--- models/transaction.go
+++ models/transaction.go
@@ -6,6 +6,6 @@ type Transaction struct {
 	ReceiverAccountNo uint64
 	TimeStamp         string
 	Amount            float64  `pg:",notnull"`
-	AccountID         uint64   `pg:"on_delete:SET NULL, notnull"`
+	AccountID         uint64   `pg:"on_delete:SET NULL, notnull, on_update:CASCADE"`
 	Account           *Account `pg:"rel:has-one"`
 }
